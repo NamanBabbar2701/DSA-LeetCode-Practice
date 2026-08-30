53fc75aca1cde2d827f6196329991ef00f3831f2
Naman Babbar <namanbabbar37@gmail.com>
2026-08-30 22:28:50 +0530

    Time: 3 ms (99.07%), Space: 51.1 MB (41.87%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,43 +1,31 @@
 class Solution {
     public int kadens(int[] nums){
-      int currentSum = nums[0];
-        int overallSum = nums[0];
+        int currSum = nums[0];
+        int overAllSum = nums[0];
 
-        for(int i = 1 ; i < nums.length; i++){
-            if(currentSum + nums[i] > nums[i]){
-                currentSum += nums[i];
-            }else currentSum = nums[i];
+        for(int i = 1; i < nums.length ; i++){
+            if(currSum + nums[i] > nums[i]){
+                currSum += nums[i];
+            }else currSum = nums[i];
 
-            overallSum = Math.max(currentSum , overallSum);
+            overAllSum = Math.max(currSum, overAllSum);
         }
 
-        return overallSum;  
+        return overAllSum;
     }
     public int maxSubarraySumCircular(int[] nums) {
-        if(nums.length == 0) return 0;
-        // int currentSum1 = nums[0];
-        // int overallSum1 = nums[0];
-
-        // for(int i = 1 ; i < nums.length; i++){
-        //     if(currentSum1 + nums[i] > nums[i]){
-        //         currentSum1 += nums[i];
-        //     }else currentSum1 = nums[i];
-
-        //     overallSum1 = Math.max(currentSum1 , overallSum1);
-        // }
         int linearSum = kadens(nums);
-        
         int totalSum = 0;
-        for(int i = 0 ; i < nums.length ; i++){
+        for(int i = 0; i < nums.length; i++){
             totalSum += nums[i];
             nums[i] *= -1;
         }
 
-        // 
         int invertedSum = kadens(nums);
 
         if(totalSum + invertedSum == 0) return linearSum;
 
-        return Math.max(linearSum, totalSum + invertedSum);
+        return Math.max(linearSum , totalSum + invertedSum);
+
     }
 }
