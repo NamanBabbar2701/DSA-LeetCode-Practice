bb869417cea50e31a23a1fd23258553f96a71c81
Naman Babbar <namanbabbar37@gmail.com>
2026-08-30 22:51:27 +0530

    Time: 3 ms (6.36%), Space: 94.5 MB (33.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,10 +4,11 @@ class Solution {
         int maxProfit = 0;
         int i = 1;
         while(i < prices.length){
-            maxProfit = Math.max(maxProfit , prices[i] - minVal);
+            maxProfit = Math.max(maxProfit, prices[i]  - minVal);
             i++;
             minVal = Math.min(minVal, prices[i-1]);
-        }       
+        }
+
         return maxProfit;
     }
 }
